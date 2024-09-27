649e0c0aee246d7f2b63c84c0c13c9b677860f4d
gaetanodigrazia <gaetano.digrazia@bitrock.it>
2024-09-27 10:36:02 +0200

    Initial commit

---
+++ src/main/java/com/digrazia/ImplicitGrantPOC/ImplicitGrantPocApplication.java
@@ -0,0 +1,13 @@
+package com.digrazia.ImplicitGrantPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplicitGrantPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplicitGrantPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digrazia/ImplicitGrantPOC/ImplicitGrantPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digrazia.ImplicitGrantPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplicitGrantPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
